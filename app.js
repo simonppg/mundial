4f9419d2bcb67eb4801e8bce4d997ffa1d2d481a
simonppg <simonppg@gmail.com>
2018-06-09 20:21:15 -0500

    Cleaning data

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const URL_PATH = "https://raw.githubusercontent.com/simonppg/mundial/master/";
-var people = JSON.parse('{"players":[], "total": 0, "completed": 0}');
+var people = JSON.parse('{"players":[], "total": -1, "completed": 0, "results": 0}');
+
+function mapper(a){
+  var myObj = new Object();
+  myObj[a[0]] = a[1];
+  return myObj;
+}
 
 function addPlayer(playerName, points) {
   var rowCount = $('#myTable tr').length;
@@ -12,21 +18,48 @@ function addPlayer(playerName, points) {
   //console.log("Adding player: "+playerName+" on row: "+rowCount);
 }
 
-function processData(csvString) {
-  var results = Papa.parse(csvString);
-  people.players.push({person: this, data: results.data});
-  people.completed++;
+function getResults(csvString) {
+  var matchResults = Papa.parse(csvString);
+  matchResults.data.splice(-1,1);
+  matchResults.data.splice(0,1);
+  people.results = matchResults.data.map(mapper);
+  people.results = Object.assign({}, ...people.results);
 
-  if(people.total == people.completed)
-  {
-    // Process the points here
-    for(var i = 0; i < people.players.length; i++){
-      addPlayer(people.players[i].person.name, 0);
+  for (var match in people.results) {
+    console.log(match);
+    for(var j = 0; j < people.players.length; j++){
+      console.log(people.results[match]);
+      console.log(people.players[j].data[match]);
+      if(people.results[match].toUpperCase() === people.players[j].data[match].toUpperCase())
+      {
+        console.log("Point for: "+people.players[j].name);
+        addPlayer(people.players[j].name, 0);
+      }
     }
   }
 }
 
-var getForecast = {
+function processData(csvString) {
+  var playerData = Papa.parse(csvString);
+  playerData.data = playerData.data.map(mapper);
+  people.players.push({name: this.name, data: playerData.data});
+  people.completed++;
+
+  if(people.total == people.completed)
+  {
+    for(var j = 0; j < people.players.length; j++){
+      people.players[j].data.splice(-1,1);
+      people.players[j].data.splice(0,1);
+      people.players[j].data = Object.assign({}, ...people.players[j].data);
+    }
+    // Get matches results
+    ajaxConfig.url = URL_PATH.concat("results.csv");
+    ajaxConfig.success = getResults;
+    $.ajax(ajaxConfig);
+  }
+}
+
+var ajaxConfig = {
   type: "GET",
   dataType: "text",
 }
@@ -36,10 +69,10 @@ function fillFilesNames(data) {
   for(var i = 0; i < lines.length - 1; i++){
     var person = new Object();
     person.name = lines[i];
-    getForecast.url = URL_PATH.concat(lines[i]).concat(".csv");
-    getForecast.success = processData;
-    getForecast.context = person;
-    $.ajax(getForecast)
+    ajaxConfig.url = URL_PATH.concat(lines[i]).concat(".csv");
+    ajaxConfig.success = processData;
+    ajaxConfig.context = person;
+    $.ajax(ajaxConfig)
   }
 
   people.total = lines.length - 1;
